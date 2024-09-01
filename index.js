095cc9642c98323fd77789903f346b028a019e56
Aiden Cullo <aidencullo@gmail.com>
2024-09-01 19:02:02 -0400

    code to get a random dog

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Use the fetch API or Axios to communicate with an external web API. Use the data provided by this API to populate your application’s content and features.
+
+const getRandomDogBtn = document.querySelector('#getRandomDogBtn');
+const dogImage = document.querySelector('#dogImage');
+
+getRandomDogBtn.addEventListener('click', () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then((response) => response.json())
+    .then((data) => {
+      dogImage.src = data.message;
+      dogImage.style.display = 'block';
+    });
+});
