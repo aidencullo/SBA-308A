a7d3f62ed3d0b169bd8f852531b0e9fbfcba8b8f
Aiden Cullo <aidencullo@gmail.com>
2024-09-02 17:45:48 -0400

    split to fns

--- utils/random.js
+++ utils/random.js
@@ -1,5 +1,5 @@
 const dogImage = document.querySelector('#dogImage');
-const dogStatus = document.querySelector('#dogStatus');
+const loadingStatus = document.querySelector('#dogStatus');
 const dogInfo = document.querySelector('#dogInfo');
 const getRandomDogBtn = document.querySelector('#dog-btn');
 
@@ -9,28 +9,32 @@ const LOADING_MESSAGE = 'Your dog is loading...';
 
 const getRandomDog = async () => {
   try {
-
-    dogStatus.textContent = LOADING_MESSAGE;
-    dogImage.src = '';
-    dogImage.style.height = '0';
-    dogImage.style.width = '0';
-
-    const response = await fetch(RANDOM_DOG_API);
-    const data = await response.json();
-
-    dogImage.style.height = '200px';
-    dogImage.style.width = '200px';
-    dogImage.src = data.message;
-    dogStatus.textContent = '';
-
-    dogInfo.innerHTML = `
-			<p>Breed: ${data.message.split('/')[4]}</p>
-			<p>Sub-breed: ${data.message.split('/')[5]}</p>
-		`;
-
+    startLoading();
+    const data = await fetchRandomDog();
+    publishDog(data);
   } catch (error) {
     dogImage.src = LOADING_DOG_IMAGE;
   }
+};
+
+const startLoading = () => {
+  loadingStatus.textContent = LOADING_MESSAGE;
+  dogImage.src = '';
+  dogImage.style.height = '0';
+  dogImage.style.width = '0';
+};
+
+const fetchRandomDog = async () => {
+  const response = await fetch(RANDOM_DOG_API);
+  const data = await response.json();
+  return data;
+};
+
+const publishDog = (data) => {  
+  dogImage.style.height = '200px';
+  dogImage.style.width = '200px';
+  dogImage.src = data.message;
+  loadingStatus.textContent = '';
 };
 
 getRandomDog();
