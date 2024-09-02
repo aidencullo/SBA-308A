b9cd71b1a19e390b4a4673b05b2a2b8320145bd2
Aiden Cullo <aidencullo@gmail.com>
2024-09-02 10:36:22 -0400

    set dog info

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dogImage = document.querySelector('#dogImage');
 const dogStatus = document.querySelector('#dogStatus');
+const dogInfo = document.querySelector('#dogInfo');
 
 const LOADING_DOG_IMAGE = 'https://via.placeholder.com/300';
 const RANDOM_DOG_API = 'https://dog.ceo/api/breeds/image/random';
@@ -7,16 +8,25 @@ const LOADING_MESSAGE = 'Your dog is loading...';
 
 const getRandomDog = async () => {
   try {
+
     dogStatus.textContent = LOADING_MESSAGE;
     dogImage.src = '';
     dogImage.style.height = '0';
     dogImage.style.width = '0';
+
     const response = await fetch(RANDOM_DOG_API);
     const data = await response.json();
+
     dogImage.style.height = '200px';
     dogImage.style.width = '200px';
     dogImage.src = data.message;
     dogStatus.textContent = '';
+
+    dogInfo.innerHTML = `
+			<p>Breed: ${data.message.split('/')[4]}</p>
+			<p>Sub-breed: ${data.message.split('/')[5]}</p>
+		`;
+
   } catch (error) {
     dogImage.src = LOADING_DOG_IMAGE;
   }
