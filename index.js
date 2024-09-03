01f769c27e8b99cce219746c5100a7cd86d6972e
Aiden Cullo <aidencullo@gmail.com>
2024-09-02 22:31:14 -0400

    stub out incorrect guesses call

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const CAT_API = 'https://api.thecatapi.com/v1';
+const CAT_API_KEY = 'live_85IigfDFRAJz3RZl3AHEGeioejA1FeoZe5RpLo7Si7yYzbLATq0UWuocM3qAqRJC';
+
 import showPhotos from './utils/cats.js';
 import showOptions from './utils/breeds.js';
 
@@ -27,6 +30,37 @@ const handleFormSubmit = async () => {
   showOptions(answer);
 };
 
+const getRandBreed = async () => {
+  const breeds = await fetch(`${CAT_API}/breeds`)
+  const data = await breeds.json()
+  const rand = Math.floor(Math.random() * data.length)
+  return data[rand].name
+}
+
+const getIncorrectGuesses = async () => {
+  // const breed = await getFavorites()
+  return 'standin breed'
+}
+
+const showWrongGuesses = async () => {
+  const breed = await getIncorrectGuesses();
+  const wrongGuesses = document.createElement('div');
+  const title = document.createElement('h1');
+  title.textContent = 'Wrong Guesses';
+  wrongGuesses.appendChild(title);
+
+  // Create list and list item
+  const wrongGuessesList = document.createElement('ul');
+  const wrongGuessesListItem = document.createElement('li');
+  wrongGuessesListItem.textContent = breed;
+  wrongGuessesList.appendChild(wrongGuessesListItem);
+  wrongGuesses.appendChild(wrongGuessesList);
+
+  // Append to container
+  const container = document.getElementById('container');
+  container.appendChild(wrongGuesses);
+};
+
 const appendFormElements = () => {
   const input = createInputElement();
   const button = createSubmitButton();
@@ -35,5 +69,6 @@ const appendFormElements = () => {
 };
 
 appendFormElements();
+showWrongGuesses();
 
 export { createInputElement, createSubmitButton, handleFormSubmit, appendFormElements };
