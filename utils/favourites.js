7ba38b43944420b8569c62a0cde1c26aecf4cda9
Aiden Cullo <aidencullo@gmail.com>
2024-09-02 23:33:17 -0400

    i think an image of the incorrectly guessed breed is being added to favourites, not sure

--- utils/favourites.js
+++ utils/favourites.js
@@ -1,3 +1,7 @@
+const CAT_API_KEY = 'live_85IigfDFRAJz3RZl3AHEGeioejA1FeoZe5RpLo7Si7yYzbLATq0UWuocM3qAqRJC';
+
+
+
 const createFavourite = async (imgId) => {
   const baseUrl = 'https://api.thecatapi.com/v1/favourites';
   const headers = {
@@ -22,11 +26,32 @@ const createFavourite = async (imgId) => {
 
 const addToFavourites = async (breed) => {
   try {
-    await createFavourite(imgId);
-    alert('Image added to favourites');
+    const randomImageId = await getRandomImageId(breed);
+    const fav = await createFavourite(randomImageId);
   } catch (error) {
     alert('An error occurred while adding the image to favourites');
   }
+}
+
+const getRandomImageId = async (breed) => {
+  const baseUrl = 'https://api.thecatapi.com/v1/images/search';
+  const headers = {
+    'x-api-key': CAT_API_KEY,
+  };
+
+  const params = {
+    breed_id: breed,
+  };
+
+  const url = new URL(baseUrl);
+  url.search = new URLSearchParams(params).toString();
+
+  const response = await fetch(url, {
+    headers: headers,
+  });
+  const data = await response.json();
+
+  return data[0].id;
 }
 
 export { addToFavourites };
